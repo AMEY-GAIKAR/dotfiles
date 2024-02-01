22df862f1073ead207de3d32097657dd328a97a8
Amey <ameygaikar28@gmail.com>
2024-02-01 20:07:35 +0530

    theme

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "catppuccin",
+  theme = "rosepine",
   theme_toggle = { "catppuccin", "rosepine" },
 
   hl_override = highlights.override,
