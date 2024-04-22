4fc34d0db15f883ff845aff8be8959d52cbd4c77
Amey <ameygaikar28@gmail.com>
2024-04-22 16:09:20 +0530

    add dmenu

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "rosepine",
+  theme = "catppuccin",
   theme_toggle = { "catppuccin", "rosepine" },
 
   hl_override = highlights.override,
